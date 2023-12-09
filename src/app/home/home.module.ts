69809d14d90c00b98af76370e44bfd4f8b9068c7
Ashwani Mishra <ashwani01superman@gmail.com>
2023-12-10 02:08:05 +0530

    course component genrattion

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { CoursesComponent } from './courses/courses.component';
 
 
 
 @NgModule({
   declarations: [
     LandingpageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    CoursesComponent
   ],
   imports: [
     CommonModule
